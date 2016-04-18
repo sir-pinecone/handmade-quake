e7f860fee09ed61916b039729c0798720000a6ba
Michael Campagnaro <mikecampo@gmail.com>
2016-04-17 23:25:30 -0400

    Fix type mismatch warning

--- module-1/code/q_stl.h
+++ module-1/code/q_stl.h
@@ -1,7 +1,7 @@
 #pragma once
 
 
-uint32 Q_strlen(uint8 *str);
+uint32 Q_strlen(const char *str);
 void Q_strcpy(char *dest, const char *src);
 void Q_strncpy(char *dest, const char *src, int32 count);
 int32 Q_strcmp(const char *s1, const char *s2);
